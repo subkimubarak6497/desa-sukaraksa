3def3fdfca739615ca112342b68f32698e1cf907
subkimubarak6497 <149258253+subkimubarak6497@users.noreply.github.com>
2023-10-29 14:17:29 +0700

    Update struktur desa

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/struktur-desa-sukaraksa.png" alt="" />
+      <img src="@/assets/struktur-desa-sukaraksa.png.jpeg" alt="" />
     </div>
   </main>
 </template>
