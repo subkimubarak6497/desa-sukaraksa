10ddd5e583e2a9a43dc230a3fc544473b7c095eb
subkimubarak6497 <149258253+subkimubarak6497@users.noreply.github.com>
2023-10-29 17:34:41 +0700

    Update penambahan visi dan misi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -54,7 +54,49 @@ import Sidebar from "../components/Sidebar.vue";
           majemuk dan dinamis;
         </li>
         <li>
-![alt text](Data-kependudukan-sukaraksa.jpg.jpeg?raw=true)        
+<div class="flex-1 p-10">
+      <p class="text-3xl font-semibold">Visi & Misi Desa Sukaraksa</p>
+      <p class="text-lg font-semibold">Visi</p>
+      <p>
+        “Maju dan Sejahtera Bersama Rakyat ”
+      </p>
+      <p class="text-lg font-semibold">Misi</p>
+      <ul>
+        <li>
+          1. Meningkatkan Pembangunan Infrastruktur yang Profesional dan berkelanjutan;
+        </li>
+        <li>
+          2. Menyelenggarakan pendidikan melalui pelatihan secara periodik;
+        </li>
+        <li>
+          3. Menjadikan Desa pengiringan yang cerdas, maju, mandiri dan sejahtera;
+        </li>
+        <li>
+          4. Peduli terhadap kepentingan masyarakat dan lingkungan;
+        </li>
+        <li>
+          5. Membina dan memantapkan kehidupan gotong royongsebagai basis dan
+          modal dasar pembangunan desa, yang pada hakikatnya juga merupakan
+          karakter dasar dari masyarakat desa dan juga bangsa Indonesia;
+        </li>
+        <li>
+          6. Memberikan pelayanan pembuatan administrasi kependudukan, seperti pengantar KTP, pengantar Kartu Keluarga dan Linnya ;
+        </li>
+        <li>
+          7. Menyalurkan segala bentuk bantuan dari Pemerintah Kabupaten,
+          Provinsi dan Pusat sesuai dengan peruntukannya dan mencegah terjadinya
+          penyimpangan dan kebocoran;
+        </li>
+        <li>
+          8. Menjalin sinergitas dengan instansi atau Lembaga terkait dan pihak
+          swasta guna optimalisasi suatu tujuan lebih baik dan lebih maksimal;
+        </li>
+        <li>
+          9. Responsive dan aspiratif atas segala bentuk kebutuhan atau
+          keinginan dan permasalahan yang terjadi ditengah masyarakat yang
+          majemuk dan dinamis;
+        </li>
+        <li>        
 
 <style>
 body {
